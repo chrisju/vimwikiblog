ed9c740b5554defe318c49ff3111fd49a48e6d6e
chrisju <mchrisju@gmail.com>
2013-08-28 10:55:13 +0800

    ok

---
+++ html/disqus.js
@@ -0,0 +1,13 @@
+//<!-- disqus -->
+
+document.write('<div id="disqus_thread"></div>');
+
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'zzwiki'; // required: replace example with your forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+ var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+ dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+ (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+ })();
